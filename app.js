ff34f91f555ba09756409b55d47094917cebb8ea
Mark <markstopkidding@gmail.com>
2023-02-24 11:38:43 +0700

    Learned switch

--- app.js
+++ app.js
@@ -1,25 +1,22 @@
- /* Вася положил 12 000 на вклад 7% годовых
- с капитализацией 1 раз в месяц.
- Вывести в консоль, сможет ли он купить дом
- за 13 500 через 2 года после снятия вклада.
- И остаток после покупки.
+const secretNumber = Number(prompt('Введите число:'));
+if (secretNumber===7) {
+    console.log('Good');
 
-Итог = Сумма * (1 + Ставка в месяц не в %) ** срок в месяцах */
-
-//данные
-let depo = 12000;
-let cost = 13500;
-let term = 24;
-let incomeMonth = 0.07/12;
-let income = depo * (1 + incomeMonth) ** 24;
-
-//результат
-if (income>cost) {
-    console.log(`You earned: ${income}
-You can buy house
-Your Residual balance: ${income - cost}`)
 } else {
-    console.log(`Can not buy house
-You earned: ${income}`)
+    console.log('Bad');
 }
 
+const role = prompt("Write your role:")
+switch (role) {
+    case "admin":
+        console.log("Admin");
+        break; 
+    case "manager":
+        console.log("Manager");
+        break;
+    case "ceo":
+        console.log("CEO");
+        break;
+    default:
+        console.log("We don't know you!")
+}
