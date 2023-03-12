344770be2f409fbc4e32168df88f9e46097ff652
Mark <markstopkidding@gmail.com>
2023-03-12 19:04:48 +0700

    Binary Array To Number

--- app.js
+++ app.js
@@ -1,21 +1,27 @@
-function rps (p1, p2) {
-  let res;
-  if (p1 === 'rock' && p2 === 'scissors') {
-    res = 'Player 1 won!';
-  } else if (p1 === 'scissors' && p2 === 'rock') {
-    res = 'Player 2 won!';
-  } else if (p1 === 'paper' && p2 === 'rock') {
-    res = 'Player 1 won!';
-  } else if (p1 === 'rock' && p2 === 'paper') {
-    res = 'Player 2 won!';
-  } else if (p1 === 'scissors' && p2 === 'paper') {
-    res = 'Player 1 won!';
-  } else if (p1 === 'paper' && p2 === 'scissors') {
-    res = 'Player 2 won!';
-  } else if (p1 === p2) {
-    res = 'Draw!'
+function binaryArrayToNumber(arr) {
+  let rev = arr.reverse();
+  res = [];
+  if (rev[0] === 1) {
+    res.push(1);
+  } else if (rev[0] === 0) {
+    res.push(0)
+  } else return 'error';
+
+  for (let i = 1; i<rev.length; i++) {
+    if (rev[i] === 1) {
+      res.push(2**i)
+    } else if (rev[i] === 0) {
+      res.push(0)
+    } else {
+      return 'error'
+    }
   }
-  return res;
+
+  let sum = 0;
+  for (let i = 0; i<rev.length; i++) {
+    sum += res[i]
+  }
+  return sum;
 }
 
-console.log(rps('rock', 'paper'));
+console.log(binaryArrayToNumber([0, 1, 1, 0]))
